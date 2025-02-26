02d81a8f5b552ec0879a4c51240d506b9fe8b223
Spider-Man <theirlwolf@gmail.com>
2025-02-26 07:18:14 +0530

    Update and rename Config.py to config.py

--- Config.py
+++
@@ -1,8 +0,0 @@
-# config.py
-
-# Your Telegram Bot Token
-TOKEN = 'YOUR_TELEGRAM_BOT_TOKEN'  # Replace with your actual Telegram bot token
-
-# URL Shortener API details
-URL_SHORTENER_API = 'https://api.urlshortener.com/shorten'  # Replace with the actual URL shortener API endpoint
-URL_SHORTENER_API_KEY = 'YOUR_URL_SHORTENER_API_KEY'  # Replace with your actual API key
---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+
+# Your Telegram Bot Token
+TOKEN = '7798556356:AAFHcX8SNmD8S7ngKeaAWVIYkKBwYXGvzhk'  # Replace with your actual Telegram bot token
+
+# URL Shortener API details
+URL_SHORTENER_API = 'https://api.shareus.io/easy_api?key=zii2MApF3pahz6kNXg9fhTx1iot1&link=https://shareus.io'  # Replace with the actual URL shortener API endpoint
+URL_SHORTENER_API_KEY = 'zii2MApF3pahz6kNXg9fhTx1iot1'  # Replace with your actual API key
